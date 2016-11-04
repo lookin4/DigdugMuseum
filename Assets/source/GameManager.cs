320c8a8692ce1e043c6853dcf493ea6766744bb7
森ノ内亮平 <morinouchiryouhei@morinouchiryouhei-no-MacBook-Pro.local>
2016-11-04 16:24:02 +0900

    test

---
+++ Assets/source/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	static GameManager instance;
+
+	public static GameManager Instance{
+		get{return instance;}
+	}
+
+	[SerializeField]
+	GameObject moguraObj;
+
+	[SerializeField]
+	Text timeText;
+
+	[SerializeField]
+	Text scoreText;
+
+	[SerializeField]
+	Text countText;
+
+	float countTime = 5.0f;
+
+	public float time;
+	public int score = 0;
+
+	public enum SCENE{
+		COUNT,
+		MAIN,
+		END
+	}
+
+	public SCENE scene = SCENE.COUNT;
+
+	public static int moguraMax = 5;//現状奇数のみ
+
+	public List<GameObject> moguraObjList = new List<GameObject>();
+
+	void Awake(){
+		instance = this;
+	}
+
+	private void CreateMogura(){
+		int a = moguraMax / 2;
+		for (int j = -a; j <= a; j += 1) {
+			GameObject obj = Instantiate (moguraObj);
+			Vector3 pos = new Vector3 (j*2.0f,-1.0f,-45.0f);
+			obj.transform.position = pos;
+
+			moguraObjList.Add (obj);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		CreateMogura ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (scene) {
+		case SCENE.COUNT:
+			if (countTime > 3.0f) {
+				countText.text = "Ready...";
+			} else if (countTime > 0.0f) {
+				countText.text = "" + ((int)countTime + 1);
+			} else {
+				countText.text = "";
+				scene = SCENE.MAIN;
+			}
+			countTime -= 1.0f / 60.0f;
+			break;
+		case SCENE.MAIN:
+			//切り上げ表示にする為+1
+			timeText.text = "Time:" + (int)(time + 0.9f);
+			scoreText.text = "score:" + score;
+			time -= 1.0f / 60.0f;
+			if (time <= 0.0f)
+				scene = SCENE.END;
+			break;
+		case SCENE.END:
+			countText.text = "Finish!";
+			break;
+		}
+	}
+}
---
+++ Assets/source/MoguraManager.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MoguraManager: MonoBehaviour {
+
+	[SerializeField]
+	Text[] judgeText;
+
+	//textの寿命設定
+	const int lifeCount = 50;
+	int[] textLife = new int[GameManager.moguraMax];
+
+	enum STATUS{
+		STAY,
+		UP,
+		DOWN
+	}
+
+	STATUS[] moguraStatus = new STATUS[GameManager.moguraMax];
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < GameManager.moguraMax; i++) {
+			moguraStatus [i] = STATUS.STAY;
+			textLife [i] = 0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TextUpdate ();
+		switch (GameManager.Instance.scene) {
+		case GameManager.SCENE.COUNT:
+			break;
+		case GameManager.SCENE.MAIN:
+			MoguraUpdate ();
+			KeyUpdate ();
+			break;
+		case GameManager.SCENE.END:
+			MoguraUpdate ();
+			break;
+		}
+	}
+		
+
+	void Judge(int instanceNo){
+		//二重判定阻止の為
+		if (string.IsNullOrEmpty (judgeText[instanceNo].text)) {
+			//position.yは-1.0f~0.5f間で移行0.5fに近いほど高得点
+			if (GameManager.Instance.moguraObjList [instanceNo].transform.position.y > 0.4f) {
+				judgeText [instanceNo].text = "Perfect!";
+				GameManager.Instance.score += 1000;
+			} else if (GameManager.Instance.moguraObjList [instanceNo].transform.position.y > 0.2f) {
+				judgeText [instanceNo].text = "Nice!";
+				GameManager.Instance.score += 500;
+			} else {
+				judgeText [instanceNo].text = "Bad..";
+				GameManager.Instance.score += 100;
+			}
+			textLife [instanceNo] = lifeCount;
+			//ここじゃない感満載だけどボタン押下時はSTATUSをDOWNに変える。
+			moguraStatus [instanceNo] = STATUS.DOWN;
+		}
+	}
+
+
+	void MoguraUpdate(){
+		//毎フレーム処理。モグラのステータスを回す。
+		for (int i = 0; i < GameManager.moguraMax; i++) {
+			//上に出てくる処理。上限まで来たら下がる処理へ移行
+			if (moguraStatus [i] == STATUS.UP) {
+				if (GameManager.Instance.moguraObjList [i].transform.position.y <= 0.5f) {
+					Vector3 pos = GameManager.Instance.moguraObjList [i].transform.position;
+					pos.y += 0.05f;
+					GameManager.Instance.moguraObjList [i].transform.position = pos;
+				} else {
+					moguraStatus [i] = STATUS.DOWN;
+				}
+			}
+			//下に降りる処理。下限まで来たら待ち状態へ移行
+			if (moguraStatus [i] == STATUS.DOWN) {
+				if (GameManager.Instance.moguraObjList [i].transform.position.y >= -1.0f) {
+					Vector3 pos = GameManager.Instance.moguraObjList [i].transform.position;
+					pos.y -= 0.05f;
+					GameManager.Instance.moguraObjList [i].transform.position = pos;
+				} else {
+					moguraStatus [i] = STATUS.STAY;
+				}
+			}
+			//乱数を使用して待ち状態のモグラを上がる状態に移行
+			if (moguraStatus [i] == STATUS.STAY) {
+				if (Random.value > 0.995f) {
+					moguraStatus [i] = STATUS.UP;
+				}
+			}
+		}
+	}
+
+
+	void TextUpdate(){
+		//毎フレーム処理。Textの表示時間の管理。
+		for (int i = 0; i < GameManager.moguraMax; i++) {
+			if (textLife [i] > 0) {
+				textLife [i]--;
+			}
+			else if (textLife [i] <= 0 && !(string.IsNullOrEmpty(judgeText [i].text))) {
+				judgeText [i].text = "";
+			}
+		}
+	}
+
+
+	void KeyUpdate(){
+		//キー入力の制御（ここじゃないかもだから一時的に？）
+		if (Input.GetKeyDown (KeyCode.A)) {
+			if (moguraStatus [0] != STATUS.STAY) {
+				Judge(0);
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			if (moguraStatus [1] != STATUS.STAY) {
+				Judge(1);
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			if (moguraStatus [2] != STATUS.STAY) {
+				Judge(2);
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.F)) {
+			if (moguraStatus [3] != STATUS.STAY) {
+				Judge(3);
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.G)) {
+			if (moguraStatus [4] != STATUS.STAY) {
+				Judge(4);
+			}
+		}
+	}
+}
